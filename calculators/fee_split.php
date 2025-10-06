bd25cf3fadf34fad070d005b6a4f8ea55e69022d
AhmadFaizan44 <139695357+AhmadFaizan44@users.noreply.github.com>
2025-10-06 21:07:06 +0500

    Update fee_split.php

--- calculators/fee_split.php
+++ calculators/fee_split.php
@@ -3,8 +3,7 @@
 <form id="fee-form" method="post" class="calc">
   <div class="row">
     <div><label>Total Fee / Principal (PKR)</label><input id="fee-principal" name="principal" type="number" class="input" value="120000"/></div>
-    <div><label>Months</label><input id="fee-months" name="months" type="number" min="1" class="input" value="12"/>
-
+    <div><label>Months</label><input id="fee-months" name="months" type="number" min="1" class="input" value="12"/></div>
   </div>
   <div><label>Monthly Mark-up Rate (%)</label><input id="fee-rate" name="rate" type="number" step="0.01" class="input" value="0"/></div>
   <button class="btn-primary" type="submit">Calculate</button>
